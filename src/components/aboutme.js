c0644cda8acf6bce7a7b55930613c468e4421cda
lledezma <luis.ledezma@pop.belmont.edu>
2020-04-20 12:33:11 -0500

    updates

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 
 class About extends Component
 {
-    render()
-    {
-      return(
-        <div>
-          <h1>About Page</h1>
-        </div>
-      )
-    }
+  render()
+  {
+    return(
+      <div>
+        <h1>About Page</h1>
+      </div>
+    )
+  }
 }
   
 export default About;
