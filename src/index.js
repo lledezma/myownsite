beeabbf4f40e8faed809ca4d205938610079477e
lledezma <luis.ledezma@pop.belmont.edu>
2021-05-23 08:45:10 -0500

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
- <BrowserRouter>
+  <BrowserRouter>
    <App />
- </BrowserRouter>
-  , document.getElementById('root'));
-  
+ </BrowserRouter>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
